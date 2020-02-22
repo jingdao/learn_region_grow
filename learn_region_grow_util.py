186ed0241f6e038e7f851878c96ba3c6eaf6f54e
jingdao <chenjingdao@wustl.edu>
2020-02-22 15:42:20 -0500

    cleanup architecture code. fix result figures

--- learn_region_grow_util.py
+++ learn_region_grow_util.py
@@ -95,8 +95,6 @@ class LrgNet:
 		self.neighbor_pl = tf.placeholder(tf.float32, shape=(batch_size*seq_len, num_neighbor_points, feature_size))
 		self.add_mask_pl = tf.placeholder(tf.int32, shape=(batch_size*seq_len, num_neighbor_points))
 		self.remove_mask_pl = tf.placeholder(tf.int32, shape=(batch_size*seq_len, num_inlier_points))
-		self.seq_pl = tf.placeholder(tf.int32, shape=(batch_size))
-		self.seq_mask_pl = tf.placeholder(tf.bool, shape=(batch_size*seq_len))
 
 		#CONVOLUTION LAYERS FOR INLIER SET
 		for i in range(len(CONV_CHANNELS)):
@@ -118,23 +116,7 @@ class LrgNet:
 		self.pool = tf.reduce_max(self.conv[4], axis=1)
 		self.neighbor_pool = tf.reduce_max(self.neighbor_conv[4], axis=1)
 		self.combined_pool = tf.concat(axis=1, values=[self.pool, self.neighbor_pool])
-
-		#RECURRENT LAYERS (disabled if seq_len==1)
-		if seq_len > 1:
-			rnn_cell = tf.contrib.rnn.GRUCell(CONV_CHANNELS[-1]*2)
-#			initial_state = rnn_cell.zero_state(batch_size, dtype=tf.float32)
-#			initial_state = tf.Variable(rnn_cell.zero_state(batch_size, dtype=tf.float32), trainable=False)
-			self.rnn_init = tf.get_variable('rnn_init', [CONV_CHANNELS[-1]*2], initializer=tf.constant_initializer(0.0), dtype=tf.float32)
-			initial_state = tf.tile(tf.reshape(self.rnn_init,[1,CONV_CHANNELS[-1]*2]) , [batch_size, 1])
-			self.rnn, self.final_state = tf.nn.dynamic_rnn(rnn_cell, tf.reshape(self.combined_pool, [batch_size, seq_len, -1]), initial_state=initial_state, dtype=tf.float32, sequence_length = self.seq_pl)
-			print('rnn', self.rnn, initial_state, self.final_state)
-#			with tf.control_dependencies([initial_state.assign(self.final_state)]):
-#				self.rnn = tf.identity(self.rnn)
-			self.pooled_feature = tf.reshape(self.rnn, [batch_size*seq_len, -1])
-		else:
-			self.pooled_feature = self.combined_pool
-
-		##CLASSIFICATION BRANCH##
+		self.pooled_feature = self.combined_pool
 
 		#CONCAT AFTER POOLING
 		self.inlier_tile[0] = tf.tile(tf.reshape(self.pooled_feature,[batch_size*seq_len,-1,CONV_CHANNELS[-1]*2]) , [1,1,num_inlier_points])
@@ -173,19 +155,6 @@ class LrgNet:
 		self.remove_conv[i] = tf.nn.bias_add(self.remove_conv[i], self.remove_bias[i])
 		self.remove_output = self.remove_conv[i]
 
-		#calculate loss only within sequence
-		if seq_len > 1:
-			M = tf.where(self.seq_mask_pl)
-			self.add_output_seq = tf.gather_nd(self.add_output, M)
-			self.add_pl_seq = tf.gather_nd(self.add_mask_pl, M)
-			self.remove_output_seq = tf.gather_nd(self.remove_output, M)
-			self.remove_pl_seq = tf.gather_nd(self.remove_mask_pl, M)
-		else:
-			self.add_output_seq = self.add_output
-			self.add_pl_seq = self.add_mask_pl
-			self.remove_output_seq = self.remove_output
-			self.remove_pl_seq = self.remove_mask_pl
-
 		#LOSS FUNCTIONS
 		def weighted_cross_entropy(logit, label):
 			pos_mask = tf.where(tf.cast(label, tf.bool))
@@ -196,19 +165,17 @@ class LrgNet:
 			neg_loss = tf.cond(tf.is_nan(neg_loss), lambda: 0.0, lambda: neg_loss)
 			return pos_loss + neg_loss
 
-		self.add_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.add_output_seq, labels=self.add_pl_seq))
-#		self.add_loss = weighted_cross_entropy(self.add_output_seq, self.add_pl_seq)
-		self.add_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.add_output_seq, -1), tf.to_int64(self.add_pl_seq)), tf.float32))
-		TP = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(tf.argmax(self.add_output_seq, -1), 1), tf.equal(self.add_pl_seq, 1)), tf.float32))
-		self.add_prc = TP / (tf.cast(tf.reduce_sum(tf.argmax(self.add_output_seq, -1)), tf.float32) + 1)
-		self.add_rcl = TP / (tf.cast(tf.reduce_sum(self.add_pl_seq), tf.float32) + 1)
-#		self.remove_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.remove_output_seq, labels=self.remove_pl_seq))
-		self.remove_loss = weighted_cross_entropy(self.remove_output_seq, self.remove_pl_seq)
-		self.remove_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.remove_output_seq, -1), tf.to_int64(self.remove_pl_seq)), tf.float32))
-		self.remove_mask = tf.nn.softmax(self.remove_output_seq, axis=-1)[:, :, 1] > 0.5
-		TP = tf.reduce_sum(tf.cast(tf.logical_and(self.remove_mask, tf.equal(self.remove_pl_seq, 1)), tf.float32))
+		self.add_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.add_output, labels=self.add_mask_pl))
+		self.add_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.add_output, -1), tf.to_int64(self.add_mask_pl)), tf.float32))
+		TP = tf.reduce_sum(tf.cast(tf.logical_and(tf.equal(tf.argmax(self.add_output, -1), 1), tf.equal(self.add_mask_pl, 1)), tf.float32))
+		self.add_prc = TP / (tf.cast(tf.reduce_sum(tf.argmax(self.add_output, -1)), tf.float32) + 1)
+		self.add_rcl = TP / (tf.cast(tf.reduce_sum(self.add_mask_pl), tf.float32) + 1)
+		self.remove_loss = weighted_cross_entropy(self.remove_output, self.remove_mask_pl)
+		self.remove_acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.remove_output, -1), tf.to_int64(self.remove_mask_pl)), tf.float32))
+		self.remove_mask = tf.nn.softmax(self.remove_output, axis=-1)[:, :, 1] > 0.5
+		TP = tf.reduce_sum(tf.cast(tf.logical_and(self.remove_mask, tf.equal(self.remove_mask_pl, 1)), tf.float32))
 		self.remove_prc = TP / (tf.reduce_sum(tf.cast(self.remove_mask, tf.float32)) + 1)
-		self.remove_rcl = TP / (tf.cast(tf.reduce_sum(self.remove_pl_seq), tf.float32) + 1)
+		self.remove_rcl = TP / (tf.cast(tf.reduce_sum(self.remove_mask_pl), tf.float32) + 1)
 
 		self.loss = self.add_loss + self.remove_loss
 		batch = tf.Variable(0)
